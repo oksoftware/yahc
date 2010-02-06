70a29b70ae35dae8bbde41a6b4582461b1a88a41
oksoftware <oksoftware@gmail.com>
2010-02-07 03:10:03 +0900

    add AX header dumping

--- dishsp.cpp
+++ dishsp.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include "AXLexer.h"
 
@@ -23,7 +24,7 @@ class Dishsp {
 			using namespace std;
 			this->out = out;
 
-			*out<<"oksoftware HSP 3.2 IR Code Disassembler"<<endl
+			*out<<"oksoftware HSP 3.2 AX Dumper"<<endl
 			    <<"Copyright(C) 2010 oksoftware. All rights reserved."<<endl<<endl;
 			return;
 		}
@@ -46,6 +47,9 @@ class Dishsp {
 				cerr<<"Couldn't open the file."<<endl;
 				return 1;
 			}
+
+			*out<<"Dump of file "<<argv[1]<<endl<<endl;
+
 			return 0;
 		}
 
@@ -60,7 +64,40 @@ class Dishsp {
 		}
 
 		void writeHeader(AXHeader *header){
-			/**/
+			using namespace std;
+
+			*out<<"AX FILE HEADER"<<endl;
+			*out<< hex << right <<
+				"\t" << setw(8) << *((unsigned int *)header->magic) << " Magic Code 'HSP3'" << endl <<
+				"\t" << setw(8) << header->version << " Version Number Info" << endl <<
+				"\t" << setw(8) << header->maxVal << " Max Count of VAL Object" << endl <<
+				"\t" << setw(8) << header->size << " Total File Size" << endl <<
+
+				"\t" << setw(8) << header->pCS << " Ptr to Code Segment" << endl <<
+				"\t" << setw(8) << header->maxCS << " Size of Code Segment" << endl <<
+				"\t" << setw(8) << header->pDS << " Ptr to Data Segment" << endl <<
+				"\t" << setw(8) << header->maxDS << " Size of Data Segment" << endl <<
+
+				"\t" << setw(8) << header->pOT << " Ptr to Object Temp" << endl <<
+				"\t" << setw(8) << header->maxOT << " Size of Object Temp" << endl <<
+				"\t" << setw(8) << header->pDINFO << " Ptr to Debug Info" << endl <<
+				"\t" << setw(8) << header->maxDINFO << " Size of Debug Info" << endl <<
+
+				"\t" << setw(8) << header->pLINFO << " Ptr to LibInfo" << endl <<
+				"\t" << setw(8) << header->maxLINFO << " Size of LibInfo" << endl <<
+				"\t" << setw(8) << header->pFINFO << " Ptr to FuncInfo" << endl <<
+				"\t" << setw(8) << header->maxFINFO << " Size of FuncInfo" << endl <<
+
+				"\t" << setw(8) << header->pMINFO << " Ptr to ModInfo" << endl <<
+				"\t" << setw(8) << header->maxMINFO << " Size of ModInfo" << endl <<
+				"\t" << setw(8) << header->pFINFO2 << " Ptr to FuncInfo2" << endl <<
+				"\t" << setw(8) << header->maxFINFO2 << " Size of FuncInfo2" << endl <<
+
+				"\t" << setw(8) << header->pHPIDAT << " Ptr to HPIDAT" << endl <<
+				"\t" << setw(8) << header->maxHPIDAT << " Size of HPIDAT" << endl <<
+				"\t" << setw(8) << header->maxVARHPI << " Num of Vartype Plugins" << endl <<
+				"\t" << setw(8) << header->bootOption << " Bootup Options" << endl <<
+				"\t" << setw(8) << header->runtime << " Ptr to Runtime Name" << endl;
 			return;
 		}
 };
